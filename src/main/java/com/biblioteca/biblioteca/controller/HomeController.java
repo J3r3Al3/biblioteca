3c967c3af50f0c6154f3c589c767a409058de1fb
J3r3Al3 <elpro131311x@gmail.com>
2026-01-20 18:52:18 -0500

    Update HomeController.java

--- src/main/java/com/biblioteca/biblioteca/controller/HomeController.java
+++ src/main/java/com/biblioteca/biblioteca/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.biblioteca.biblioteca.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
 
