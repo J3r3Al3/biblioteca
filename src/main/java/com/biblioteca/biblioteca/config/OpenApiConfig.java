a0908ac6d50948f5f783f361ec4158ad847a9c7f
J3r3Al3 <elpro131311x@gmail.com>
2026-01-21 14:21:22 -0500

    Update OpenApiConfig.java

--- src/main/java/com/biblioteca/biblioteca/config/OpenApiConfig.java
+++ src/main/java/com/biblioteca/biblioteca/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package com.biblioteca.biblioteca.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,6 +17,9 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("📚 API Biblioteca")
                         .description("Sistema de gestión de libros - Proyecto Final POO")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .servers(List.of(
+                        new Server().url("https://biblioteca-production-1c6d.up.railway.app")
+                ));
     }
 }
